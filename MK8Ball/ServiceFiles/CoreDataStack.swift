07b742f471b746a2ad97f5b76ff07356ea7deb74
Mykola Korotun <KolyanMSKR@gmail.com>
2021-10-15 22:18:34 +0300

    Add CoreDataStack

---
+++ MK8Ball/ServiceFiles/CoreDataStack.swift
@@ -0,0 +1,34 @@
+//
+//  CoreDataStack.swift
+//  MK8Ball
+//
+//  Created by Mykola Korotun on 15.10.2021.
+//
+
+import CoreData
+
+class CoreDataStack {
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "MK8Ball")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+}
